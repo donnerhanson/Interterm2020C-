078cc093392e4d4b41daa489026754f8df0b64a3
Donner Hanson <donnerhansoncomposer@yahoo.com>
2020-01-14 09:34:26 -0800

    added firecapacity

---
+++ Problemfirecapacity/Problemfirecapacity.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  Problemfirecapacity
+//
+//  Created by Donner Hanson on 1/13/20.
+//  Copyright © 2020 Donner Hanson. All rights reserved.
+//
+/*
+ Write a program that determines whether a meeting room is
+ in violation of fire law regulations regarding the maximum room capacity.
+ 
+ The program will:
+ 1) read in the maximum room capacity and the number of
+ people to attend the meeting.
+ If the number of people is less than or equal to the
+ maximum room capacity,
+ the program announces that it is legal to hold the meeting
+ and tells how many additional people may legally attend.
+ 
+ If the number of people exceeds the maximum room capacity,
+ the program announces:
+ the meeting cannot be held as planned due to fire regulations
+ and tells how many people must be excluded in order to meet
+ the fire regulations.
+ */
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main(int argc, const char * argv[]) {
+    //only whole people allowed into the party
+    int maxCapacity;
+    int numAttending;
+    cout << "Enter max capacity of room followed by the number of people"
+            << " attending - followed by return:\n";
+    cin >> maxCapacity >> numAttending;
+    
+    if (numAttending <= maxCapacity)
+    {
+        cout << "It is legal to hold the meeting\n";
+        if (numAttending != maxCapacity && (maxCapacity - numAttending) > 1)
+            cout << maxCapacity - numAttending
+                    << " additional people may legally attend.\n";
+        else
+            cout << maxCapacity - numAttending
+            << " additional person may legally attend.\n";
+    }
+    else
+    {
+        cout << "It is illegal to hold the meeting\n";
+            cout << numAttending - maxCapacity
+                    << " people must be excluded in order to meet"
+                    << " the fire regulations.\n";
+    }
+    return 0;
+}
---
+++ Problemfirecapacity/README.md
@@ -0,0 +1,15 @@
+ASSIGNMENT CEREAL
+
+Author: Donner Hanson
+Date: 1/13/2020
+UserID: 1276484
+
+Assignment:
+A metric ton is 35,273.92 ounces. Write a program that will:  
+Read the weight (console input) of a package of breakfast cereal in ounces  
+Output the weight in metric tons  
+Output the number of boxes needed  
+to yield one metric ton of cereal.  
+
+  
+Perform all functionality within the main function and include a README and MakeFile.
