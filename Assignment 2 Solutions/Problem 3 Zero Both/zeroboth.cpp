7a9cd78111a58ee899f6e31c668ee830eefe271e
Donner Hanson <donnerhansoncomposer@yahoo.com>
2020-01-23 15:57:57 -0800

    Formatting and removing unneeded headers

--- Assignment 2 Solutions/Problem 3 Zero Both/zeroboth.cpp
+++ Assignment 2 Solutions/Problem 3 Zero Both/zeroboth.cpp
@@ -6,14 +6,15 @@ Date: 1/13/2020
 UserID: 1276484
 
 Assignment:
-Problem 3 (zeroboth.cpp): Implement a void function called zeroBoth that has two call-by- reference parameters, both of which are variables of type int, and sets the values of both variables to 0.  */
+Problem 3 (zeroboth.cpp): Implement a void function called
+ zeroBoth that has two call-by- reference parameters,
+ both of which are variables of type int, and sets
+ the values of both variables to 0.
+ */
 
-#include <cmath>
-#include <iomanip>
+
 #include <iostream>
-#include <limits>
-#include <stdexcept>
-#include <string>
+
 
 
 // USING STATEMENTS FROM RESPECTIVE LIBRARIES
