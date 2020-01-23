ebfe37440f62998a5f8163944ef205f7ffd46352
Donner Hanson <donnerhansoncomposer@yahoo.com>
2020-01-22 18:10:33 -0800

    Problem 1 complete and tested with docker

--- Assignment 2 Solutions/Problem 1 IntArray/README.md
+++ Assignment 2 Solutions/Problem 1 IntArray/README.md
@@ -8,3 +8,8 @@ Assignment:
 Problem 1 (intarray.cpp): Write a program that will read up to ten nonnegative integers into an array
 called numberArray and then write the integers back to the screen (console output).
 For this exercise you need not use any functions.
+
+References:
+https://www.geeksforgeeks.org/converting-strings-numbers-cc/
+https://stackoverflow.com/questions/20731/how-do-you-clear-a-stringstream-variable
+https://stackoverflow.com/questions/38544151/compare-two-string-as-numeric-value
--- Assignment 2 Solutions/Problem 1 IntArray/intarray.cpp
+++ Assignment 2 Solutions/Problem 1 IntArray/intarray.cpp
@@ -1,47 +1,113 @@
 /*
  ASSIGNMENT IntArray
+ 
+ Author: Donner Hanson
+ Date: 1/23/2020
+ UserID: 1276484
+ 
+ Assignment:
+ Problem 1 (intarray.cpp): Write a program that will read up to ten
+ nonnegative integers into an array called
+ numberArray and then write the integers back to the screen (console
+ output). For this exercise you need not use any functions.
+ */
 
-Author: Donner Hanson
-Date: 1/23/2020
-UserID: 1276484
-
-Assignment:
-Problem 1 (intarray.cpp): Write a program that will read up to ten
-nonnegative integers into an array called
-numberArray and then write the integers back to the screen (console
-output). For this exercise you need not use any functions.
-*/
-
-#include <cmath>
-#include <iomanip>
 #include <iostream>
 #include <limits>
-#include <stdexcept>
 #include <string>
+#include <sstream>
 
 // USING STATEMENTS FROM RESPECTIVE LIBRARIES
 //iostream
 using std::cout;
 using std::cin;
-using std::cerr;
+using std::endl;
 // limits
 using std::numeric_limits;
-// stdexcept
-using std::invalid_argument;
-using std::runtime_error;
 // string
 using std::string;
-
+using std::to_string;
+// sstream
+using std::stringstream;
 // CONST EXPRESSIONS
-
-
+const int ARR_SZ = 10;
+string MAX_INT_VAL = to_string(numeric_limits<int>::max());
 //FUNCTIONS
 
 
 // MAIN
 int main(int argc, const char * argv[]) {
+    int numberArray[ARR_SZ];
+    for (int i = 0; i < ARR_SZ; ++i)
+    {
+        numberArray [i] = -1;  // initialize all to -1
+    }
     
-
-    
+    string userIn = "";
+    int convertedNum = 0;
+    cout << "This program will output non-negative numbers"
+    << " that are entered by a user. Enter a negative number or"
+    << " alphabetic character to quit and output\n";
+    cout << "Enter first number: ";
+    stringstream strToNum("");
+    for (int i = 0; convertedNum >= 0 && i < ARR_SZ; ++i)
+    {
+        
+        cin >> userIn;
+        // check if empty and if positive integer
+        if (!userIn.empty() &&
+            userIn.find_first_not_of("0123456789") == std::string::npos)
+        {
+            if (userIn.length() == MAX_INT_VAL.length())
+            {
+                bool isTooLarge(false);
+                for (int j = 0; j < userIn.length(); ++j)
+                {
+                    // if the number is greater than the max int value break
+                    if (userIn.at(j) > MAX_INT_VAL.at(j))
+                    {
+                        isTooLarge = true;
+                        break;
+                    }
+                    // at least one digit in length is smaller - fits
+                    else if (userIn.at(j) < MAX_INT_VAL.at(j))
+                    {
+                        break;
+                    }
+                }
+                if (isTooLarge)
+                {
+                    cout << "number too large for integer storage\n";
+                    break;
+                }
+                
+            }
+            else if (userIn.length() > MAX_INT_VAL.length())
+            {
+                break;
+            }
+            // else its a good length
+            strToNum << userIn;
+            strToNum >> convertedNum;
+            numberArray[i] = convertedNum;
+            // clear eof bit to reset stream insertion
+            strToNum.clear();
+            // technically more efficient than .str("")
+            strToNum.str(std::string());
+            
+        }
+        else
+        {
+            break;
+        }
+        if (i < ARR_SZ - 1)
+            cout << "Enter next number: ";
+    }
+    // all invalid elements set to -1 - will break when -1 found
+    cout << "Stored values: " << endl;
+    for (int i = 0; i < ARR_SZ && numberArray[i] >= 0; ++i)
+    {
+        cout << numberArray[i] << endl;
+    }
     return 0;
 }
