7491b591b491a2aa71233a419fedb71f6996d4d0
Donner Hanson <donnerhansoncomposer@yahoo.com>
2020-01-23 16:14:30 -0800

    Added functionality and formmatted

--- Assignment 2 Solutions/Problem 4 Sales Tax/salestax.cpp
+++ Assignment 2 Solutions/Problem 4 Sales Tax/salestax.cpp
@@ -6,24 +6,18 @@
  UserID: 1276484
  
  Assignment:
- Problem 4 (salestax.cpp): Implement a function called addTax. The function addTax has two formal parameters: taxRate, which is the amount of sales tax expressed as a percent; and cost, which is the cost of an item before tax. The function returns the value (float) of cost so that it includes sales tax.
+ Problem 4 (salestax.cpp): Implement a function called addTax.
+ The function addTax has two formal parameters:
+ taxRate, which is the amount of sales tax expressed as a percent;
+ and cost, which is the cost of an item before tax.
+ 
+ The function returns the value (float) of cost so that it includes sales tax.
  */
-
-
-
-#include <iostream>
-#include <cmath>
 #include <iomanip>
 #include <iostream>
-#include <limits>
-#include <stdexcept>
-#include <string>
 
 
 // USING STATEMENTS FROM RESPECTIVE LIBRARIES
-// cmath
-using std::fabs;
-using std::fmod;
 // iomanip
 using std::fixed;
 using std::setprecision;
@@ -31,22 +25,26 @@ using std::setprecision;
 using std::cout;
 using std::cin;
 using std::cerr;
-// limits
-using std::numeric_limits;
-// stdexcept
-using std::invalid_argument;
-// string
-using std::string;
+using std::endl;
 
 // CONST EXPRESSIONS
+const float TAX_RATE(0.075);
+const float COST(1000);
+const int DECIMAL_PRECISION(2);
 
-
-//FUNCTIONS
-
+//FUNCTION DECLARTAIONS
+float addTax(float &taxRate, float &cost);
 
 // MAIN
 int main(int argc, const char * argv[]) {
-    
+    float taxRate(TAX_RATE), cost(COST);
+    cout << fixed << setprecision(DECIMAL_PRECISION)
+            << addTax(taxRate, cost) << endl;
     return 0;
 }
 
+// FUNCTION DEFINITIONS
+float addTax(float &taxRate, float &cost)
+{
+    return cost + cost * taxRate;
+}
